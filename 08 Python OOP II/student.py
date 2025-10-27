a157b451942451820df4d8359b1dcd2e09695be5
GeorginaShirley07 <georginashirley07@gmail.com>
2025-10-27 14:59:23 +0000

    27.10

--- 08 Python OOP II/student.py
+++ 08 Python OOP II/student.py
@@ -1,6 +1,7 @@
-def __init__(self, name, id):
-    self.name = name
-    self.id = id
+class Student:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
 
-def print_info(self):
-    print(self.name,self.id)
+    def print_info(self):
+        print(self.name,self.id)
