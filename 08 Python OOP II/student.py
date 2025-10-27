2ad35f723453c6774d51b40c4776b3142d18bec3
GeorginaShirley07 <georginashirley07@gmail.com>
2025-10-27 14:55:23 +0000

    commit

---
+++ 08 Python OOP II/student.py
@@ -0,0 +1,6 @@
+def __init__(self, name, id):
+    self.name = name
+    self.id = id
+
+def print_info(self):
+    print(self.name,self.id)
--- student.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    def __init__(self, name, id):
-        self.name = name
-        self.id = id
-
-# call the constructor with name and id
-student1 = Student("Gina", 22510977)
-print(student1.name)
-print(student1.id)
